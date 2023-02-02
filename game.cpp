4ffdef53b82724b59a91674779bdd0b2d4de4ce9
David Coleman <davidlcoleman3rd@gmail.com>
2023-02-02 17:06:41 -0500

    **Version 0.0.7**  - Began to layout the structure of the Game class, which connects the     bulk of classes together in one single "playspace" for the gameplay

--- game.cpp
+++ game.cpp
@@ -1,2 +1,36 @@
+//***********************************************
+//      GAME CLASS                              *
+//                                              *
+//***********************************************
+//                                              *
+//  This C++ class is the main source of the    *
+//  poker game.  It serves as the "playspace"   *
+//  for the game.                               *
+//                                              *
+//***********************************************
+//  Version 0.0.4.1 pushed by David Coleman III *
+//  on  01 - 24 - 23                            *
+//***********************************************
 
-//TO BE IMPLEMENTED
+
+    Game() {                       //Default constructor - creates the decks, players, and rivers
+
+    }
+
+//******
+    void PlayGame(){               //Starts the gameplay loop
+
+    }
+
+//******
+    ~Game() {                      //Deallocates the deck, players, and river pointers
+
+    }
+/*
+private:
+    Deck* dDeck;                    //The deck being used for the current game
+    River* dRiver;                  //The river being used for the current game
+    CircularList* lPlayers;         //The list of players, sorted by turn order
+
+};
+*/
